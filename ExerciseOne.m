b8be50d42167c7d221db9980d95385c51eebce07
Dylan Baker <skaduvee32@gmail.com>
2023-09-25 10:55:11 +0200

    updated prac 2 code

--- ExerciseOne.m
+++ ExerciseOne.m
@@ -1,9 +1,9 @@
 function ExerciseOne
 fs = 10000;
-t = 0:1/fs:0.15;
+t = 0:1/fs:0.2;
 %mt = zeros(length(t));
 index = 1;
-for i = 0:1/fs:0.15
+for i = 0:1/fs:0.2
     if (i <= 0.05)
         mt(index) = 4*sinc(200*i) + (10*i);
     elseif (i >= 0.05 && i <= 0.1)
@@ -13,6 +13,10 @@ for i = 0:1/fs:0.15
     end
     index = index + 1;
 end
+
+figure(10)
+obw(mt, fs)
+
 Mf = fftshift(fft(mt));
 n = length(mt) - 1;
 df = fs/n;
@@ -25,4 +29,24 @@ xlim([0 0.12])
 figure(2)
 plot(f,abs(Mf)/(length(Mf) - 1));
 xlim([-800 800])
+
+test_x = sinc(200*t);
+
+figure(40)
+plot(t,test_x);
+
+Testf = fftshift(fft(test_x));
+
+figure(41)
+plot(f,abs(Testf)/(length(Testf) - 1));
+
+ten_t = 10*t;
+Ten_tf = fftshift(fft(ten_t));
+
+figure(51)
+plot(f,abs(Ten_tf)/(length(Ten_tf) - 1));
+xlim([-1000 1000])
+
+figure(100)
+obw(ten_t, fs)
 end
--- ExerciseTwo.m
+++ ExerciseTwo.m
@@ -1,8 +1,8 @@
 function ExerciseTwo
-fs = 20000;
-t = 0:1/fs:0.15;
+fs = 50000;
+t = 0:1/fs:0.2;
 index = 1;
-for i = 0:1/fs:0.15
+for i = 0:1/fs:0.2
     if (i <= 0.05)
         mt(index) = 4*sinc(200*i) + (10*i);
     elseif (i >= 0.05 && i <= 0.1)
@@ -13,12 +13,18 @@ for i = 0:1/fs:0.15
     index = index + 1;
 end
 
+%figure(11)
+%obw(mt,fs)
+
 fc = 2000;
 Kf = 999;
 
 ut = fmmod(mt,fc,fs,Kf);
 
 ut = 2 * ut;
+
+figure(10)
+obw(ut,fs)
 
 figure(1)
 plot(t,ut);
@@ -30,14 +36,25 @@ figure(2)
 plot(t,demod_ut);
 xlim([0 0.1])
 
-noise = wgn(1, length(ut), 0.01,'linear');
+Uf = fftshift(fft(ut));
+n = length(ut) - 1;
+df = fs/n;
+f = -fs/2:df:fs/2;
 
-ut_with_noise = ut + noise;
+figure(3)
+plot(f,abs(Uf)/(length(Uf) - 1));
+
+noise = wgn(1, length(ut), 0.01,'linear');
+filtered_noise = bandpass(noise, [1000 1750], fs);
+
+ut_with_noise = ut + filtered_noise;
 
 figure(3)
 plot(t, ut_with_noise);
 
+
 demod_noise_ut = fmdemod(ut_with_noise, fc, fs, Kf);
 figure(4)
 plot(t, demod_noise_ut);
+title("demodulated with noise")
 end
